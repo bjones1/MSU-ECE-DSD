c13cfb24c48406073728c3f00ef6af81162f4482
bjones1 <bjones@ece.msstate.edu>
2022-08-19 08:07:25 -0500

    Update: Document and clean lab 1 files.

--- lab1/lab1.v
+++ lab1/lab1.v
@@ -1,30 +1,38 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    21:08:32 08/15/2014 
-// Design Name: 
-// Module Name:    lab1 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+// <h1><code>lab1.v</code> - design source for lab 1</h1>
+// <p>See the lab instructions to use this file for simulation and for
+//     programming the Basys 3 board.</p>
+// <p>This is a design source file, which means that all the "software"
+//     in this file will be synthesized into hardware, so that it can
+//     then be programmed on the FPGA. Therefore, make sure you
+//     understand what hardware lies behind each line of "code". In
+//     contrast, the simulation source file <code><a
+//             href="tb_lab1.v.html">tb_lab1.v</a></code> will not by
+//     synthesized, and can contain code that's not synthesizable.</p>
+
+// <p>A Verilog <a
+//         href="https://www.chipverify.com/verilog/verilog-modules">module</a>
+//     defines a component in a typical schematic. <a
+//         href="https://www.chipverify.com/verilog/verilog-ports">Ports</a>,
+//     which consist of inputs and/or outputs, allow the module to
+//     connect to external devices. The top-level module in a Xilinx
+//     design connects directly to pins on the FPGA; the <code><a
+//             href="Basys3_Master.xdc.html">Basys3_Master.xdc</a></code>&nbsp;file
+//     maps these names (<code>LED</code> and <code>SW</code>) to
+//     physical pins on the FPGA.&nbsp;</p>
 module lab1(
-output [7:0] LED,
-input [7:0] SW
-    );
+    // <p>This defines outputs from the module: an array of 8 wires. (All
+    //     ports are wires by default).</p>
+    output [7:0] LED,
+    // <p>This defines inputs to the module, also an array of 8 wires.</p>
+    input [7:0] SW
+);
 
-//this connects the first 8 switches to the first 8 LEDs 
-//SW0 is the right most switch, and order is right to left
-assign LED = SW; 
+// <p>The <a
+//         href="https://www.chipverify.com/verilog/verilog-assign-statement">assign</a>
+//     statement makes a wire-like connection. In this case, it connects
+//     the first 8 switches to the first 8 LEDs. SW0 is the right-most
+//     switch on the Basys 3 board; the order is right to left.</p>
+assign LED = SW;
 
+// <p>Every module must contain this statement.</p>
 endmodule
--- lab1/tb_lab1.v
+++ lab1/tb_lab1.v
@@ -1,49 +1,104 @@
+// <h1>tb_lab1.v - Test bench for the <code><a
+//             href="lab1.v.html">lab1.v</a></code> module</h1>
+// <p>This file provides a series of tests (called a test bench) to
+//     verify that the <code><a href="lab1.v.html">lab1.v</a></code>
+//     module works correctly. Because this file will not be synthesized,
+//     it can contain any code, including code that cannot be
+//     synthesized.</p>
+// <p>The timescale <a
+//         href="https://www.chipverify.com/verilog/verilog-timescale">compiler
+//         directive</a> specifies the time unit and precision for one
+//     time unit. Therefore, the <a href="#100-delay">#100 delay
+//         statement</a> which appears later in this file specifies a
+//     delay of 100 ns.</p>
 `timescale 1ns / 1ps
 
+// <p>In contrast to <code><a href="lab1.v.html">lab1.v</a></code>, this
+//     module contains no inputs or outputs; therefore, the ports are
+//     omitted.</p>
 module tb_leds;
 
-	// Inputs
-	reg [7:0] sw;
+    // <p>For testing, make the switches a register. Like variables in
+    //     traditional programming languages, <span
+    //         style="text-decoration: underline;"><strong><code>reg</code></strong></span>isters
+    //     may contain a value.</p>
+    reg [7:0] sw;
 
-	// Outputs
-	wire [7:0] led;
-	
-	integer i;
-	integer errors = 0;
+    // <p>The outputs only need to be compared to the inputs, but don't need
+    //     to store a value. Declare them as wires.</p>
+    wire [7:0] led;
+    
+    // <p>An <code>integer</code> is essentially a 32-bit register. It's a
+    //     convenient way to declare and use 32-bit values.</p>
+    integer i;
+    integer errors = 0;
 
-	// Instantiate the Unit Under Test (UUT)
-	lab1 uut (
-		.LED(led), 
-		.SW(sw)
-	);
+    // <p><a
+    //         href="https://www.chipverify.com/verilog/verilog-module-instantiations">Instantiate</a>
+    //     the Unit Under Test (UUT). This means:</p>
+    // <ul>
+    //     <li>Place a copy of the <code>lab1</code> module here; name it
+    //         <code>uut</code>.</li>
+    //     <li>Connect <code>lab1</code>'s <code>LED</code> outputs to the
+    //         wires (declared a few lines earlier in this file) named
+    //         <code>led</code>.</li>
+    //     <li>Likewise, connect <code>lab1</code>'s <code>SW</code> inputs
+    //         to the registers named <code>sw</code> (also declared a few
+    //         lines earlier in this file).</li>
+    // </ul>
+    lab1 uut (
+        .LED(led), 
+        .SW(sw)
+    );
 
-	initial begin
-		// Initialize Inputs
-		sw = 0;
+    // <p>The <a
+    //         href="https://www.chipverify.com/verilog/verilog-initial-block"><code>initial</code>
+    //         block</a> defines what this module does starting at time 0;
+    //     using <code>initial begin</code>...<code>end</code> allows that
+    //     definition to include multiple statements. That is, Verilog's
+    //     <code>begin</code>...<code>end</code> serves the same role a
+    //     C/C++'s <code>{</code>...<code>}</code>.This block can only be
+    //     used in simulation, not in synthesis.</p>
+    initial begin
+        // <p>Define the inital value for all inputs.</p>
+        sw = 0;
 
-		// Wait 100 ns for global reset to finish
-		#100;
-		$display("Applying Vectors\n");
-		// Add stimulus here
-		i = 0;
-		errors = 0;
-		while (i != 256) begin
-			sw = i;  //assign i to switch input
-			#50;		 //delay;
-			if (led != i) begin
-				errors = errors + 1;
-			end //end if
-			i = i + 1;
-		end //end begin
-		
-		if (errors == 0) begin
-			$display("PASS: All test vectors passed\n");
-		end
-		else begin
-			$display("FAIL: %d errors occurred\n", errors);
-		end
-			
-
-	end
+        // <p><a id="delay-100"></a>Wait 100 ns for global reset to finish.</p>
+        #100;
+        
+        // <p>A <a
+        //         href="https://www.chipverify.com/verilog/verilog-display-tasks">display</a>
+        //     statement acts much like a <code>printf</code> statement in C or
+        //     <code>cout</code> in C++ by printing output to the screen.</p>
+        $display("Applying vectors...\n");
+        
+        // <p>To test the lab1 module, apply all possible inputs (the values 0 to
+        //     255) and check that each output matches with the input.</p>
+        // <p>Begin by initializing some variables.</p>
+        i = 0;
+        errors = 0;
+        // <p>Use a <code>for</code> loop to check each possible value. Note that
+        //     Verilog doesn't have the increment operator, so the expression
+        //     <code>++i</code> is invalid. Likewise, it doesn't allow inplace
+        //     addition, making <code>i += 1</code> invalid as well. Instead, use
+        //     <code>i = i + 1</code>.</p>
+        for (i = 0; i != 256; i = i + 1) begin
+            // Assign i to the switch input.
+            sw = i;  
+            // Delay, to allow for propoagation time.
+            #50;
+            // <p>Check that the output matches the input.</p>
+            if (led != i) begin
+                errors = errors + 1;
+            end
+        end 
+        
+        // <p>Error check after the loop completes.</p>
+        if (errors == 0) begin
+            $display("PASS: All test vectors passed\n");
+        end else begin
+            $display("FAIL: %d errors occurred\n", errors);
+        end
+    end
       
 endmodule
