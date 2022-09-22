266d8b9e56f57c05da91fd462fc0e272325ceab2
bjones1 <bjones@ece.msstate.edu>
2022-09-22 17:19:09 -0500

    Fix: Improve lab 5.

--- lab5/lab5dpath.v
+++ lab5/lab5dpath.v
@@ -1,12 +1,42 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////
-module lab5dpath(x1,x2,x3,y,clk);
-input clk;
-input [9:0] x1,x2,x3;
-output [9:0] y;
+// <h1>Lab 5</h1>
+// <p>Complete this Verilog module so that it implements the following
+//     schematic:</p>
+// <figure class="image"><img src="lab5datapath.png"
+//         alt="A 12x12 multiplier and summer">
+//     <figcaption>Figure 1: Schematic to implement.</figcaption>
+// </figure>
+// <p>Busses <code>k1</code>, <code>k2</code>, <code>k3</code> are the
+//     following constants:</p>
+// <table style="border-collapse: collapse;" border="1">
+//     <tbody>
+//         <tr>
+//             <td>Constant</td>
+//             <td>12-bit value in hex</td>
+//             <td>Value in decimal (in 1.11 fixed point)</td>
+//         </tr>
+//         <tr>
+//             <td><code>k1</code></td>
+//             <td>C00</td>
+//             <td>-0.5</td>
+//         </tr>
+//         <tr>
+//             <td><code>k2</code></td>
+//             <td>500</td>
+//             <td>0.625</td>
+//         </tr>
+//         <tr>
+//             <td><code>k3</code></td>
+//             <td>C00</td>
+//             <td>-0.5</td>
+//         </tr>
+//     </tbody>
+// </table>
+module lab5dpath(
+    input clk,
+	input [9:0] x1,
+	input [9:0] x2,
+	input [9:0] x3,
+	output [9:0] y
+);
 
-
- 
 endmodule
-
--- lab5/tb_lab5dpath.v
+++ lab5/tb_lab5dpath.v
@@ -1,139 +1,107 @@
+// <h1><a href="lab5dpath.v">Lab 5</a> testbench</h1>
 `timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 
 module tb_lab5dpath;
 
-	// Inputs
-	reg [9:0] x1;
-	reg [9:0] x2;
-	reg [9:0] x3;
+    // <p>Inputs</p>
+    reg clk;
+    reg [9:0] x1;
+    reg [9:0] x2;
+    reg [9:0] x3;
 
-	// Outputs
-	wire [9:0] y;
-	
-	reg clock;
-	
-	reg[8*100:1] aline;
+    // <p>Outputs</p>
+    wire [9:0] y;
 
+    // <p>Internal values</p>
+    reg [8*100:1] aline;
+    integer fd;
+    integer i_a, i_b, i_c, i_result;
+    integer errors;
 
-`define FSIZE 1024
-`define LATENCY 2    
+    // <p>Instantiate the Unit Under Test (UUT)</p>
+    lab5dpath uut (
+        .x1(x1),
+        .x2(x2),
+        .x3(x3),
+        .clk(clk),
+        .y(y)
+    );
 
+    // <p>After 100 ns delay for reset, produce a 50 ns period clock.</p>
+    initial begin
+        clk = 0;
+        #100
+        forever #25 clk = ~clk;
+    end
 
-	integer infifo[(`FSIZE-1):0];
-	integer head,tail;
-   integer fd;
-	integer count,status;
-   integer i_a, i_b, i_c, i_result;
-	integer o_a, o_b, o_c, o_result;
-	integer errors;
-	integer clock_count;
+    initial begin
+        // <p>Initialize Inputs</p>
+        x1 = 0;
+        x2 = 0;
+        x3 = 0;
 
-	
-
-	// Instantiate the Unit Under Test (UUT)
-	lab5dpath uut (
-		.x1(x1), 
-		.x2(x2), 
-		.x3(x3), 
-		.clk(clock),
-		.y(y)
-	);
-	
-	initial begin
-	 clock = 0;
-	 #100   //reset delay
-	 forever #25 clock = ~clock;
-	end
-
-	initial begin
-		// Initialize Inputs
-		x1 = 0;
-		x2 = 0;
-		x3 = 0;
-		head = 0;
-		tail = 0;
-
-		clock_count = 0;
-
-		fd = $fopen("../../../../multadd_vectors.txt","r");
-        if (fd == 0) begin
-          //for post-route simulation, one directory deeper
+        fd = $fopen("../../../../multadd_vectors.txt","r");
+        if (fd === 0) begin
+          // <p>for post-route simulation, one directory deeper</p>
           fd = $fopen("../../../../../multadd_vectors.txt","r");
         end
-        
-        if (fd == 0) begin
+
+        if (fd === 0) begin
           $display("Cannot open vectors file 'multadd_vectors.txt', simulation exiting");
           $finish;
         end
-        
-		count = 1;
 
-		// Wait 100 ns for global reset to finish
-		#100;
-	        
-		// Add stimulus here
-		// Add stimulus here
-		errors = 0;
-		while ($fgets(aline,fd)) begin
-		 status = $sscanf(aline,"%x %x %x %x",i_a, i_b, i_c, i_result);
-		 @(negedge clock);
-		 x1 = i_a;
-		 x2 = i_b;
-		 x3 = i_c;
-		 infifo[head]=i_a;inc_head;
-		 infifo[head]=i_b;inc_head;
-		 infifo[head]=i_c;inc_head;
-		 infifo[head]=i_result;inc_head;
-		
-		end //end while
-		
-		while (head != tail) begin
-		  @(negedge clock);
-		end
-		if (errors == 0) $display("PASS: All vectors passed.\n");
-         else $display ("FAIL: %d vectors failed\n",errors);
-			
-	end
-	
-	task inc_head;
-	 begin
-	  head = head + 1;
-	  if (head == `FSIZE) head = 0;	 
-	 end
-	endtask
-	
-	task inc_tail;
-	 begin
-	  tail = tail + 1;
-	  if (tail == `FSIZE) tail = 0;	 
-	 end
-	endtask
-	
-	always @(negedge clock) begin
-	  clock_count = clock_count + 1;
-	  
-	  if ((clock_count > `LATENCY+1) && (head != tail)) begin
-	  	 o_a = infifo[tail];inc_tail;
-		 o_b = infifo[tail];inc_tail;
-		 o_c = infifo[tail];inc_tail;
-		 o_result = infifo[tail];inc_tail;
-		 
-		 if (o_result == y) begin
-		  $display("%d(%t): PASS, x1: %x, x2: %x, x3: %x, y: %x\n",count,$time,o_a,o_b,o_c,y);
-		 end else begin
-        $display("%d(%t): FAIL, x1: %x, x2: %x, x3: %x, y (actual): %x, y (expected): %x\n",count,$time,o_a,o_b,o_c,y,o_result);	 
-		   errors = errors + 1;
-		 end
-		 count = count + 1;
+        // <p>Wait 100 ns for global reset to finish</p>
+        #100;
 
-		end //end if
-		
-	 
-	  end
-	      
+        // <p>Start a task to check each line in the simulus file.</p>
+        errors = 0;
+        while ($fgets(aline, fd)) begin
+            if ($sscanf(aline, "%x %x %x %x", i_a, i_b, i_c, i_result) !== 4) begin
+                $display("Error reading test vectors.");
+                $finish;
+            end
+            @(negedge clk);
+            check_mult(i_a, i_b, i_c, i_result);
+        end
+
+        // <p>Wait for the final few checks to complete.</p>
+        pipeline_wait();
+        if (errors === 0)
+            $display("PASS: All vectors passed.\n");
+        else
+            $display("FAIL: %d vectors failed\n", errors);
+        $finish;
+    end
+
+    // <p>Wait enough clocks for the pipelined multiply/add process to
+    //     produce a result.</p>
+    task pipeline_wait;
+        begin
+            @(negedge clk);
+            @(negedge clk);
+        end
+    endtask
+
+    // <p>Check if the output of the multiply/add agrees with the provided
+    //     expected value.</p>
+    task check_mult(
+        input [9:0] a,
+        input [9:0] b,
+        input [9:0] c,
+        input [9:0] y_expected
+    );
+        begin
+            x1 = a;
+            x2 = b;
+            x3 = c;
+            pipeline_wait();
+            if (y_expected === y) begin
+                $write("PASS %t: x1: %x, x2: %x, x3: %x, y: %x\n", $time, a, b, c, y);
+            end else begin
+                $write("FAIL %t: x1: %x, x2: %x, x3: %x, y actual: %x, y expected: %x\n", $time, a, b, c, y, y_expected);
+                errors = errors + 1;
+            end
+        end
+    endtask
 endmodule
-
-
