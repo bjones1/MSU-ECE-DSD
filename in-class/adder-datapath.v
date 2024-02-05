b9755573692b37b7cf3e7a39fefacdab80b0c0c2
Bryan A. Jones <bjones1@users.noreply.github.com>
2024-02-04 18:00:44 -0600

    Clean: Update files to Markdown.

--- in-class/adder-datapath.v
+++ in-class/adder-datapath.v
@@ -1,6 +1,6 @@
-// <h1>An in-class exercise on datapaths</h1>
-// <p>This module demonstrates various methods for adding three numbers.
-// </p>
+// # An in-class exercise on datapaths
+//
+// This module demonstrates various methods for adding three numbers.
 module adder_datapath(
     input clk,
     input [15:0] a,
@@ -9,83 +9,68 @@ module adder_datapath(
     output [15:0] y
 );
 
-// <h2>Simple solution</h2>
-// <p>Comment out the following line to remove the simple solution from
-//     this file. See <a
-//         href="../verilog_standard_1364-2005.pdf#page=379">Compiler
-//         directives</a>.</p>
+// ## Simple solution
+//
+// Comment out the following line to remove the simple solution from this file.
+// See [Compiler directives](../verilog_standard_1364-2005.pdf#page=379).
 `define SIMPLE
 `ifdef SIMPLE
-    // <p>The obvious approach to create an adder: just add everything
-    //     together.
+    // The obvious approach to create an adder: just add everything together.
     assign y = a + b + c;
 
-    // <p>Simulating this on the implemented design shows the
-    //     worst-case delay:</p>
-    // <figure class="image"><img src="adder-datapath-simple-sim.png" alt=""
-    //         width="938" height="213">
-    //     <figcaption>Figure 1: Post-implementation timing simulation of the
-    //         simple adder.</figcaption>
-    // </figure>
-    // <p>However, the timing analysis shows that over half the delay comes
-    //     from moving signals around the FPGA (net delay), not due to the
-    //     time taken to add the numbers.</p>
-    // <figure class="image"><img src="adder-datapath-simple-timing.png"
-    //         alt="Timing results for a adder with no flip-flops"
-    //         width="673" height="193">
-    //     <figcaption>Table 1: Timing results from an adder with no
-    //         sequential logic.</figcaption>
-    // </figure>
-    // <p>Note that the timing analysis shows a longer delay than the
-    //     simulation &ndash; the simulation didn't happen to select
-    //     worst-case inputs (such as a carry of every bit).</p>
+    // Simulating this on the implemented design shows the worst-case delay:
+    //
+    // <figure class="image"><img src="adder-datapath-simple-sim.png" alt="" width="938" height="213"><figcaption>Figure 1: Post-implementation timing simulation of the simple adder.</figcaption></figure>
+    //
+    // However, the timing analysis shows that over half the delay comes from
+    // moving signals around the FPGA (net delay), not due to the time taken to
+    // add the numbers.
+    //
+    // <figure class="image"><img src="adder-datapath-simple-timing.png" alt="Timing results for a adder with no flip-flops" width="673" height="193"><figcaption>Table 1: Timing results from an adder with no sequential logic.</figcaption></figure>
+    //
+    // Note that the timing analysis shows a longer delay than the simulation –
+    // the simulation didn't happen to select worst-case inputs (such as a carry
+    // of every bit).
 `endif
 
-// <h2>Register-based solution</h2>
-// <p>Likewise -- comment out the following line to remove the registers
-//     solution.</p>
+// ## Register-based solution
+//
+// Likewise -- comment out the following line to remove the registers solution.
 //`define REGISTERS
 `ifdef REGISTERS
-    // <p>So, an obvious fix is to use registers to hold the inputs and
-    //     outputs, reducing the net delay.</p>
-    // <figure class="image"><img src="adder-datapath-registers.png"
-    //         width="317" height="151">
-    //     <figcaption>Figure 2: An adder with registers on the input and
-    //         outputs.</figcaption>
-    // </figure>
+    // So, an obvious fix is to use registers to hold the inputs and outputs,
+    // reducing the net delay.
+    //
+    // <figure class="image"><img src="adder-datapath-registers.png" width="317" height="151"><figcaption>Figure 2: An adder with registers on the input and outputs.</figcaption></figure>
     reg [15:0] y;
-    reg [15:0] aq, bq, cq;
+    reg [15:0] aq, bq, cq;hghg
 
-    // <p>Include registers for the inputs. We don't need a reset, since this
-    //     inputs will (eventually) overwrite them with correct values.</p>
+    // Include registers for the inputs. We don't need a reset, since this
+    // inputs will (eventually) overwrite them with correct
+    // values.
     always @(posedge clk) begin
         aq <= a;
         bq <= b;
         cq <= c;
     end
 
-    // <p>Compute the sum.</p>
+    // Compute the sum.
     wire [15:0] sum;
     assign sum = aq + bq + cq;
 
-    // <p>Then provide a register for the output.</p>
+    // Then provide a register for the output.
     always @(posedge clk) begin
         y <= sum;
     end
 
-    // <p>This produces much better results:</p>
-    // <figure class="image"><img src="adder-datapath-registers-timing.png"
-    //         alt="" width="709" height="191">
-    //     <figcaption>Table 2: Timing results when using registered at the
-    //         inputs and outputs.</figcaption>
-    // </figure>
-    // <p>Now, the logic consumes about 60% of the total delay. However, the
-    //     sum now requires several clock cycles to compute:</p>
-    // <figure class="image"><img src="adder-datapath-registers-sim.png"
-    //         alt="" width="567" height="307">
-    //     <figcaption>Figure 3: Post-implementation timing simulation of the
-    //         registered adder.</figcaption>
-    // </figure>
+    // This produces much better results:
+    //
+    // <figure class="image"><img src="adder-datapath-registers-timing.png" alt="" width="709" height="191"><figcaption>Table 2: Timing results when using registered at the inputs and outputs.</figcaption></figure>
+    //
+    // Now, the logic consumes about 60% of the total delay. However, the sum
+    // now requires several clock cycles to compute:
+    //
+    // <figure class="image"><img src="adder-datapath-registers-sim.png" alt="" width="567" height="307"><figcaption>Figure 3: Post-implementation timing simulation of the registered adder.</figcaption></figure>
 `endif
 
 
--- lab4/lab4dpath.v
+++ lab4/lab4dpath.v
@@ -1,36 +1,12 @@
-// <h1>Lab 4</h1>
-// <p>Complete this Verilog module so that it implements the following
-//     schematic:</p>
-// <figure class="image"><img src="lab4dpath.png"
-//         alt="A 12x12 multiplier and summer">
-//     <figcaption>Figure 1: Schematic to implement.</figcaption>
-// </figure>
-// <p>Busses <code>k1</code>, <code>k2</code>, <code>k3</code> are the
-//     following constants:</p>
-// <table style="border-collapse: collapse;" border="1">
-//     <tbody>
-//         <tr>
-//             <td>Constant</td>
-//             <td>12-bit value in hex</td>
-//             <td>Value in decimal (in 1.11 fixed point)</td>
-//         </tr>
-//         <tr>
-//             <td><code>k1</code></td>
-//             <td>C00</td>
-//             <td>-0.5</td>
-//         </tr>
-//         <tr>
-//             <td><code>k2</code></td>
-//             <td>500</td>
-//             <td>0.625</td>
-//         </tr>
-//         <tr>
-//             <td><code>k3</code></td>
-//             <td>C00</td>
-//             <td>-0.5</td>
-//         </tr>
-//     </tbody>
-// </table>
+// # Lab 4
+//
+// Complete this Verilog module so that it implements the following schematic:
+//
+// <figure class="image"><img src="lab4dpath.png" alt="A 12x12 multiplier and summer"><figcaption>Figure 1: Schematic to implement.</figcaption></figure>
+//
+// Busses `k1`, `k2`, `k3` are the following constants:
+//
+// <table style="border-collapse: collapse;" border="1"><tbody><tr><td>Constant</td><td>12-bit value in hex</td><td>Value in decimal (in 1.11 fixed point)</td></tr><tr><td><code>k1</code></td><td>C00</td><td>-0.5</td></tr><tr><td><code>k2</code></td><td>500</td><td>0.625</td></tr><tr><td><code>k3</code></td><td>C00</td><td>-0.5</td></tr></tbody></table>
 module lab4dpath(
 	input signed [9:0] x1,
 	input signed [9:0] x2,
@@ -38,22 +14,18 @@ module lab4dpath(
 	output signed [9:0] y
 );
 
-    // <h2>Implementation Hints</h2>
-    // <ul>
-    //     <li>You may only use assignment statements or component
-    //         instantiations, you may not use an always block (sequential
-    //         statements).</li>
-    //     <li>You will need to expand the input values by adding two LSbs
-    //         with values of zero. This can be done using concatenation as
-    //         follows:</li>
-    // </ul>
+    // ## Implementation Hints
+    //
+    // - You may only use assignment statements or component instantiations, you
+    //   may not use an always block (sequential statements).
+    // - You will need to expand the input values by adding two LSbs with values
+    //   of zero. This can be done using concatenation as follows:
     wire signed [11:0] v1;
     assign v1 = {x1, 2'b00};
-    // <p>When you have to drop bits, just choose which bits you want to keep
-    //     by the bus indices. For example, the following statement drops the
-    //     two LSbs of <code>s2</code> to form <code>y</code>.</p>
+    // When you have to drop bits, just choose which bits you want to keep by
+    // the bus indices. For example, the following statement drops the two LSbs
+    // of `s2` to form `y`.
     assign y = s2[11:2];
-    // <p>You may remove or edit these lines of code &ndash; they are only
-    //     hints.</p>
+    // You may remove or edit these lines of code – they are only hints.
 
 endmodule
