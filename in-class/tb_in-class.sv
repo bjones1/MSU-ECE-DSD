85e8a6ac40f2904494b3237f642f3207f4adb394
Bryan A. Jones <bjones1@users.noreply.github.com>
2024-02-12 08:53:13 -0600

    Docs: Reformat using Markdown.

--- in-class/tb_in-class.sv
+++ in-class/tb_in-class.sv
@@ -1,33 +1,27 @@
-// <h1>tb_in-class.v - Test bench for the <code><a
-//             href="in-class.v">in-class.v</a></code> module</h1>
+// # tb_in-class.v - Test bench for the <code><a href="in-class.v">in-class.v</a></code> module
 `timescale 1ns / 1ps
 
 module tb_in_class;
-    // <p>Inputs to UUT</p>
+    // Inputs to UUT
     logic clk, reset;
     logic [15:0] a, b, c;
-    // <p>Outputs from UUT</p>
+    // Outputs from UUT
     logic [15:0] x, y, z;
     
 
-    // <p>An <code>integer</code> is essentially a 32-bit register. It's a
-    //     convenient way to declare and use 32-bit values.</p>
+    // An `integer` is essentially a 32-bit register. It's a convenient way to
+    // declare and use 32-bit values.
     integer i;
     integer errors = 0;
 
-    // <p><a
-    //         href="https://www.chipverify.com/verilog/verilog-module-instantiations">Instantiate</a>
-    //     the Unit Under Test (UUT). This means:</p>
-    // <ul>
-    //     <li>Place a copy of the <code>lab1</code> module here; name it
-    //         <code>uut</code>.</li>
-    //     <li>Connect <code>lab1</code>'s <code>LED</code> outputs to the
-    //         wires (declared a few lines earlier in this file) named
-    //         <code>led</code>.</li>
-    //     <li>Likewise, connect <code>lab1</code>'s <code>SW</code> inputs
-    //         to the registers named <code>sw</code> (also declared a few
-    //         lines earlier in this file).</li>
-    // </ul>
+    // [Instantiate](https://www.chipverify.com/verilog/verilog-module-instantiations)
+    // the Unit Under Test (UUT). This means:
+    //
+    // - Place a copy of the `lab1` module here; name it `uut`.
+    // - Connect `lab1`'s `LED` outputs to the wires (declared a few lines
+    //   earlier in this file) named `led`.
+    // - Likewise, connect `lab1`'s `SW` inputs to the registers named `sw`
+    //   (also declared a few lines earlier in this file).
     in_class uut (
         .clk(clk),
         .reset(reset),
@@ -39,7 +33,7 @@ module tb_in_class;
         .z(z)
     );
 
-    // <p>After 100 ns delay for reset, produce a 50 ns period clock.</p>
+    // After 100 ns delay for reset, produce a 50 ns period clock.
     initial begin
         clk = 0;
         #100
@@ -47,24 +41,24 @@ module tb_in_class;
     end
     
     initial begin
-        // <p>Define the inital value for all inputs.</p>
+        // Define the inital value for all inputs.
         a = 0;
         b = 0;
         c = 0;
         reset = 1;
 
-        // <p><a></a>Wait 100 ns for global reset to finish.</p>
+        // <a></a>Wait 100 ns for global reset to finish.
         #100;
 
-        // <p>A <a
-        //         href="https://www.chipverify.com/verilog/verilog-display-tasks">display</a>
-        //     statement acts much like a <code>printf</code> statement in C or
-        //     <code>cout</code> in C++ by printing output to the screen.</p>
+        // A [display](https://www.chipverify.com/verilog/verilog-display-tasks)
+        // statement acts much like a `printf` statement in C or `cout` in C++
+        // by printing output to the screen.
         $display("Applying vectors...\n");
 
-        // <p>To test the lab1 module, apply all possible inputs (the values 0 to
-        //     255) and check that each output matches with the input.</p>
-        // <p>Begin by initializing some variables.</p>
+        // To test the lab1 module, apply all possible inputs (the values 0
+        // to 255) and check that each output matches with the input.
+        //
+        // Begin by initializing some variables.
         i = 0;
         errors = 0;
         reset = 0;
@@ -76,7 +70,7 @@ module tb_in_class;
             #50;
         end
 
-        // <p>Error check after the loop completes.</p>
+        // Error check after the loop completes.
         if (errors == 0) begin
             $display("PASS: All test vectors passed\n");
         end else begin
