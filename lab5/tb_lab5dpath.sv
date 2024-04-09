7381878f37dcafc9e712cc5ea05f40dd690be1d1
Bryan A. Jones <bjones1@users.noreply.github.com>
2024-04-09 16:59:35 -0500

    Clean: Whitespace.

--- lab5/tb_lab5dpath.sv
+++ lab5/tb_lab5dpath.sv
@@ -23,34 +23,35 @@ module tb_lab5dpath;
 
     // Instantiate the Unit Under Test (UUT)
     lab5dpath uut (
-        .x1(x1),
-        .x2(x2),
-        .x3(x3),
+        .x1 (x1),
+        .x2 (x2),
+        .x3 (x3),
         .clk(clk),
-        .y(y)
+        .y  (y)
     );
 
     // After 100 ns delay for reset, produce a 50 ns period clock.
     initial begin
         clk = 0;
-        #100
-        forever #25 clk = ~clk;
+        #100 forever #25 clk = ~clk;
     end
 
     property p1;
         logic [9:0] tmp;
-        @(negedge clk) (1, tmp = y_expected) ##(`LATENCY - 1) (y === tmp);
+        @(negedge clk) (1,
+        tmp = y_expected
+        ) ##(`LATENCY-1) (y === tmp);
     endproperty
 
-    assert property (p1)
-        begin
-            $write("PASS %t: x1: %x, x2: %x, x3: %x, y: %x %x\n", $time, x1, x2, x3, y, y_expected);
-        end else begin
-            $write("FAIL %t: x1: %x, x2: %x, x3: %x, y actual: %x, y expected: %x\n", $time, x1, x2, x3, y, y_expected);
-            errors = errors + 1;
-        end
-     
-    
+    assert property (p1) begin
+        $write("PASS %t: x1: %x, x2: %x, x3: %x, y: %x %x\n", $time, x1, x2, x3, y, y_expected);
+    end else begin
+        $write("FAIL %t: x1: %x, x2: %x, x3: %x, y actual: %x, y expected: %x\n", $time, x1, x2,
+               x3, y, y_expected);
+        errors = errors + 1;
+    end
+
+
     initial begin
         // Initialize Inputs
         x1 = 0;
@@ -59,21 +60,23 @@ module tb_lab5dpath;
 
         fd = $fopen("../../../../multadd_vectors.txt", "r");
         if (fd === 0) begin
-          // for post-route simulation, one directory deeper
-          fd = $fopen("../../../../../multadd_vectors.txt", "r");
+            // for post-route simulation, one directory deeper
+            fd = $fopen("../../../../../multadd_vectors.txt", "r");
         end
 
         if (fd === 0) begin
-          $display("Cannot open vectors file 'multadd_vectors.txt', simulation exiting");
-          $finish;
+            $display("Cannot open vectors file 'multadd_vectors.txt', simulation exiting");
+            $finish;
         end
 
         // Wait 100 ns for global reset to finish
         #100;
 
-        // Start a task to check each line in the simulus file.
+        // Start a task to check each line in the stimulus file.
         errors = 0;
-        while ($fgets(aline, fd)) begin
+        while ($fgets(
+            aline, fd
+        )) begin
             if ($sscanf(aline, "%x %x %x %x", x1, x2, x3, y_expected) !== 4) begin
                 $display("Error reading test vectors.");
                 $finish;
@@ -87,10 +90,8 @@ module tb_lab5dpath;
         @(negedge clk);
         @(negedge clk);
         @(negedge clk);
-        if (errors === 0)
-            $display("PASS: All vectors passed.\n");
-        else
-            $display("FAIL: %d vectors failed\n", errors);
+        if (errors === 0) $display("PASS: All vectors passed.\n");
+        else $display("FAIL: %d vectors failed\n", errors);
         $finish;
     end
 endmodule
