aaa9c76512713bd59a0d5d3613aa08f56bf06bf7
Bryan A. Jones <bjones1@users.noreply.github.com>
2024-04-09 17:02:16 -0500

    Clean: Whitespace, SystemVerilog updates.

--- lab6/lab6_files_part1/lab6dpath.sv
+++ lab6/lab6_files_part1/lab6dpath.sv
@@ -1,7 +1,14 @@
-module lab6dpath(reset, clk, irdy, ordy, din, dout);
-	input reset, clk, irdy;
-	input signed [9:0] din;
-	output signed [9:0] dout;
-	output ordy;
+module lab6dpath (
+    reset,
+    clk,
+    irdy,
+    ordy,
+    din,
+    dout
+);
+    input reset, clk, irdy;
+    input signed [9:0] din;
+    output signed [9:0] dout;
+    output ordy;
 
 endmodule
