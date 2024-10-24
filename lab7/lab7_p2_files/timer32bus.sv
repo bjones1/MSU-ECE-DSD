5b3ff83d0abdc3893ab32aba856edca619425af4
Bryan A. Jones <bjones1@users.noreply.github.com>
2024-10-24 18:08:17 -0500

    Clean: Fix indentation.

--- lab7/lab7_p2_files/timer32bus.sv
+++ lab7/lab7_p2_files/timer32bus.sv
@@ -1,24 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 
-// Design Name: 
-// Module Name:    timer32bus 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module timer32bus(
+module timer32bus (
     clk,
     reset,
     din,
@@ -28,14 +9,14 @@ module timer32bus(
     addr
 );
 
-    input clk, reset, wren,rden;
+    input clk, reset, wren, rden;
     input [31:0] din;
     output [31:0] dout;
-    input [23:0] addr;    //24 bit address 
-    
-    //20-bit decode, compare against addr[23:4]
-    parameter TMR1_RANGE = 20'h9250A;   //20 bit decode
-    parameter TMR2_RANGE = 20'h3C74D;   //20 bit decode
-    
-endmodule
+    // 24 bit address
+    input [23:0] addr;
 
+    // 20-bit decode, compare against addr[23:4]
+    parameter TMR1_RANGE = 20'h9250A;
+    parameter TMR2_RANGE = 20'h3C74D;
+
+endmodule
